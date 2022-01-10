2265901ae1c2251f2ab1fd307b495add22651c33
Vincent <79623093+V1nc3ntWasTaken@users.noreply.github.com>
2022-01-09 21:22:21 -0600

    Initial commit

---
+++ src/main/java/me/v1nc3ntwastaken/dynmaplocationmap/DynmapLocationMap.java
@@ -0,0 +1,82 @@
+package me.v1nc3ntwastaken.dynmaplocationmap;
+
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.StringUtil;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public final class DynmapLocationMap extends JavaPlugin implements CommandExecutor, TabCompleter {
+    static FileConfiguration config;
+    DynmapLocationMap plugin;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+        saveDefaultConfig();
+
+        getCommand("map").setExecutor(new MapCommand());
+        getCommand("mapadmin").setExecutor(this);
+
+        config = getConfig();
+        plugin = this;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 1) {
+            List<String> list = new ArrayList<>();
+
+            List<String> tabComplete = new ArrayList<>();
+            tabComplete.add("reload");
+
+            StringUtil.copyPartialMatches(args[0], tabComplete, list);
+
+            return list;
+        }
+
+        return Collections.emptyList();
+    }
+
+    public DynmapLocationMap getInstance() {
+        return plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length > 0)
+            if (args[0].equalsIgnoreCase("reload")) {
+                reloadConfiguration();
+                sender.sendMessage(ChatColor.GRAY + " |" + ChatColor.DARK_GRAY + "--------------------");
+                sender.sendMessage(ChatColor.GRAY + " |" + ChatColor.RED + "  Plugin reloaded!");
+                sender.sendMessage(ChatColor.GRAY + " |" + ChatColor.DARK_GRAY + "--------------------");
+                return true;
+            }
+
+        return false;
+    }
+
+    private void reloadConfiguration() {
+        config = YamlConfiguration.loadConfiguration(new File(getDataFolder() + File.pathSeparator + "config.yml"));
+    }
+
+    public static FileConfiguration getConfiguration() {
+        return config;
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
---
+++ src/main/java/me/v1nc3ntwastaken/dynmaplocationmap/MapCommand.java
@@ -0,0 +1,72 @@
+package me.v1nc3ntwastaken.dynmaplocationmap;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.chat.ComponentSerializer;
+import org.bukkit.Color;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+import java.util.Arrays;
+
+public class MapCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        String link = DynmapLocationMap.getConfiguration().getString("dynmap-link", "https://localhost:8080/");
+
+        TextComponent beginningSpace = new TextComponent(" |");
+        beginningSpace.setColor(ChatColor.GRAY);
+
+        TextComponent textSpacer = new TextComponent("   ");
+
+        if (sender instanceof ConsoleCommandSender) {
+            TextComponent mapLinkText = new TextComponent("Map Link: ");
+            mapLinkText.setColor(ChatColor.RED);
+
+            TextComponent mapLink = new TextComponent();
+            mapLink.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, link));
+            mapLink.setColor(ChatColor.AQUA);
+
+            sender.sendMessage(ChatColor.GRAY + " |" + ChatColor.DARK_GRAY + "--------------------");
+            sender.sendMessage(ChatColor.GRAY + " |" + ChatColor.RED + "  Available map links:");
+            sender.spigot().sendMessage(beginningSpace, textSpacer, mapLinkText, mapLinkText);
+            sender.sendMessage(ChatColor.GRAY + " |" + ChatColor.DARK_GRAY + "--------------------");
+
+            return true;
+        } else {
+            Entity entity = (Entity) sender;
+
+            String locationLink = link + "?" +
+                    "worldname=" + entity.getWorld().getName() + "&" +
+                    "mapname=flat" + "&" +
+                    "zoom=4" + "&" +
+                    "x=" + entity.getLocation().getBlockX() + "&" +
+                    "y=64" + "&" +
+                    "z=" + entity.getLocation().getBlockZ();
+
+            TextComponent mapLinkText = new TextComponent("Map Link");
+            mapLinkText.setColor(ChatColor.AQUA);
+            mapLinkText.setUnderlined(true);
+            mapLinkText.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, link));
+
+            TextComponent locationLinkText = new TextComponent("Location Link");
+            locationLinkText.setColor(ChatColor.AQUA);
+            locationLinkText.setUnderlined(true);
+            locationLinkText.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, locationLink));
+
+            sender.sendMessage(ChatColor.GRAY + " |" + ChatColor.DARK_GRAY + "--------------------");
+            sender.sendMessage(ChatColor.GRAY + " |" + ChatColor.RED + "  Available map links:");
+            sender.spigot().sendMessage(beginningSpace, textSpacer, mapLinkText);
+            sender.spigot().sendMessage(beginningSpace, textSpacer, locationLinkText);
+            sender.sendMessage(ChatColor.GRAY + " |" + ChatColor.DARK_GRAY + "--------------------");
+
+            return true;
+        }
+    }
+}
